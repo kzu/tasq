7724f4607650bbebc55675481516537b42ce12ec
Daniel Cazzulino <daniel@cazzulino.com>
2010-07-30 12:53:03 -0300

    Added conditional triggers support, which allows to enable/disable a trigger when another trigger fires.

---
+++ Trunk/Tasq.Tests/TriggerExtensionsSpec.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Tasq.Tests
+{
+	public class TriggerExtensionsSpec
+	{
+		[TestClass]
+		public abstract class GivenAConditionalTrigger
+		{
+			protected abstract ITrigger CreateConditionalTrigger(ITrigger target, ITrigger condition);
+
+			[TestMethod]
+			public void WhenTargetIsDisabled_ThenSequenceIsDisabled()
+			{
+				var target = new Mock<ITrigger>();
+				target.SetupProperty(x => x.IsEnabled, false);
+				var condition = new Mock<ITrigger>();
+				condition.SetupProperty(x => x.IsEnabled, false);
+
+				var trigger = CreateConditionalTrigger(target.Object, condition.Object);
+
+				Assert.IsFalse(trigger.IsEnabled);
+			}
+
+			[TestMethod]
+			public void WhenSequenceIsDisabled_ThenBothAreDisabled()
+			{
+				var target = new Mock<ITrigger>();
+				target.SetupProperty(x => x.IsEnabled, true);
+				var condition = new Mock<ITrigger>();
+				condition.SetupProperty(x => x.IsEnabled, true);
+
+				var trigger = CreateConditionalTrigger(target.Object, condition.Object);
+
+				trigger.IsEnabled = false;
+
+				Assert.IsFalse(target.Object.IsEnabled);
+				Assert.IsFalse(condition.Object.IsEnabled);
+			}
+
+			[TestMethod]
+			public void WhenConditionIsEnabled_ThenConditionIsEnabledAndTargetIsDisabled()
+			{
+				var target = new Mock<ITrigger>();
+				target.SetupProperty(x => x.IsEnabled, false);
+				var condition = new Mock<ITrigger>();
+				condition.SetupProperty(x => x.IsEnabled, true);
+
+				var trigger = CreateConditionalTrigger(target.Object, condition.Object);
+
+				trigger.IsEnabled = true;
+
+				Assert.IsFalse(target.Object.IsEnabled);
+				Assert.IsTrue(condition.Object.IsEnabled);
+			}
+		}
+
+		[TestClass]
+		public class GivenAnEnabledWhenTrigger : GivenAConditionalTrigger
+		{
+			protected override ITrigger CreateConditionalTrigger(ITrigger target, ITrigger condition)
+			{
+				return target.EnableWhen(condition);
+			}
+
+			[TestMethod]
+			public void WhenConditionFires_ThenTargetIsEnabled()
+			{
+				var target = new Mock<ITrigger>();
+				target.SetupProperty(x => x.IsEnabled, true);
+				var condition = new Mock<ITrigger>();
+				condition.SetupProperty(x => x.IsEnabled, false);
+
+				var trigger = target.Object.EnableWhen(condition.Object);
+
+				condition.Raise(x => x.Fired += null, EventArgs.Empty);
+
+				Assert.IsTrue(target.Object.IsEnabled);
+			}
+
+			[TestMethod]
+			public void WhenConditionFiresThenTargetFires_ThenTriggerFires()
+			{
+				var target = new Mock<ITrigger>();
+				target.SetupProperty(x => x.IsEnabled, false);
+				var condition = new Mock<ITrigger>();
+				condition.SetupProperty(x => x.IsEnabled, false);
+
+				var trigger = target.Object.EnableWhen(condition.Object);
+				var fired = false;
+				trigger.Fired += (sender, args) => fired = true;
+				trigger.IsEnabled = true;
+
+				target.Raise(x => x.Fired += null, EventArgs.Empty);
+				condition.Raise(x => x.Fired += null, EventArgs.Empty);
+
+				Assert.IsTrue(fired);
+			}
+		}
+
+		[TestClass]
+		public class GivenAnDisabledWhenTrigger : GivenAConditionalTrigger
+		{
+			protected override ITrigger CreateConditionalTrigger(ITrigger target, ITrigger condition)
+			{
+				return target.DisableWhen(condition);
+			}
+
+			[TestMethod]
+			public void WhenConditionFires_ThenTargetIsDisabled()
+			{
+				var target = new Mock<ITrigger>();
+				target.SetupProperty(x => x.IsEnabled, true);
+				var condition = new Mock<ITrigger>();
+				condition.SetupProperty(x => x.IsEnabled, false);
+
+				var trigger = target.Object.DisableWhen(condition.Object);
+
+				condition.Raise(x => x.Fired += null, EventArgs.Empty);
+
+				Assert.IsFalse(target.Object.IsEnabled);
+			}
+		}
+	}
+}
---
+++ Trunk/Tasq/TriggerExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tasq
+{
+	public static class TriggerExtensions
+	{
+		public static ITrigger EnableWhen(this ITrigger target, ITrigger condition)
+		{
+			return new ConditionalTrigger(target, condition, true);
+		}
+
+		public static ITrigger DisableWhen(this ITrigger target, ITrigger condition)
+		{
+			return new ConditionalTrigger(target, condition, false);
+		}
+
+		public class ConditionalTrigger : ITrigger
+		{
+			public event EventHandler Fired = (sender, args) => { };
+
+			public ConditionalTrigger(ITrigger target, ITrigger condition, bool targetStatusOnConditionFire)
+			{
+				this.Target = target;
+				this.Condition = condition;
+
+				target.IsEnabled = false;
+
+				this.Condition.Fired += (sender, args) => this.Target.IsEnabled = targetStatusOnConditionFire;
+				this.Target.Fired += (sender, args) => this.Fired(this, EventArgs.Empty);
+			}
+
+			public bool IsEnabled
+			{
+				get { return this.Condition.IsEnabled; }
+				set
+				{
+					this.Condition.IsEnabled = value;
+					if (!value)
+						this.Target.IsEnabled = false;
+				}
+			}
+
+			public ITrigger Target { get; private set; }
+			public ITrigger Condition { get; private set; }
+		}
+	}
+}
